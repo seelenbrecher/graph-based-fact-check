c11283f86edfcc1e594c6215c8a01b46c4b48a1a
Anab <maulanaanab@gmail.com>
2021-05-06 13:16:47 +0800

    init ka-kgat

---
+++ ka-kgat/test.sh
@@ -0,0 +1,25 @@
+# python test.py --outdir ./output \
+# --test_path ../data/bert_eval.json \
+# --bert_pretrain ../bert_base \
+# --checkpoint ../checkpoint/kgat/model.best.pt \
+# --name dev.json
+
+# python test.py --outdir ./output \
+# --test_path ../data/bert_test.json \
+# --bert_pretrain ../bert_base \
+# --checkpoint ../checkpoint/kgat/model.best.pt \
+# --name test.json
+
+
+
+CUDA_VISIBLE_DEVICES=8 python test.py --outdir ./output/ \
+--test_path ../data/bert_eval.json \
+--bert_pretrain ../bert_base \
+--checkpoint ../checkpoint/reproduce-kgat/model.best.pt \
+--name rep-dev.json
+
+CUDA_VISIBLE_DEVICES=8 python test.py --outdir ./output/ \
+--test_path ../data/bert_test.json \
+--bert_pretrain ../bert_base \
+--checkpoint ../checkpoint/reproduce-kgat/model.best.pt \
+--name rep-test.json
